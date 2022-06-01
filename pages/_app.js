cde1290a6814a3908bce703209a5757b79476216
Kirk Morris <worker.kirk@gmail.com>
2022-05-31 17:49:09 -0700

    Initial commit, some homepage.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,62 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useState } from 'react';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [navOpen, setNavOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="primary-nav">
+        <header>
+          openhaven.io
+        </header>
+
+        <button
+          onClick={() => setNavOpen(!navOpen)}
+          className={navOpen ? 'nav-open' : ''}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div className={`nav-menu${navOpen ? ' nav-open' : ''}`}>
+          <Link href="/">
+            <a>
+              <img src="/icons/home.svg" alt="" />
+              <label>Home</label>
+            </a>
+          </Link>
+          <Link href="/about">
+            <a>
+              <img src="/icons/about.svg" alt="" />
+              <label>About Us</label>
+            </a>
+          </Link>
+          <Link href="/games">
+            <a>
+              <img src="/icons/games.svg" alt="" />
+              <label>Play Games</label>
+            </a>
+          </Link>
+          <Link href="/publish">
+            <a>
+              <img src="/icons/publish.svg" alt="" />
+              <label>Publish a Game</label>
+            </a>
+          </Link>
+        </div>
+      </nav>
+
+      <div
+        onClick={() => setNavOpen(false)}
+        className={`fade${navOpen ? ' nav-open' : ''}`}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
